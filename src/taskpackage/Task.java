711c37f3a4f02c23981c68305c790ca7b3521bf7
rgupta8493 <r.gupta8493@ufl.edu>
2016-04-14 14:22:35 +0200

    List task
    Added ability to list out current todo tasks

--- src/taskpackage/Task.java
+++ src/taskpackage/Task.java
@@ -1,5 +1,35 @@
 package taskpackage;
 
 public class Task {
+	
+	private int id;
+	private String description;
+	
 
+	
+
+	public Task(int idCounter, String text) {
+		this.id = idCounter;
+		this.description = text;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- src/taskpackage/TaskManager.java
+++ src/taskpackage/TaskManager.java
@@ -1,6 +1,8 @@
 package taskpackage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,18 +19,20 @@ public class TaskManager {
 	static void menu()
 	{
 		
+		String choiceText= "";
 		int choice = 0;
 		do{
 			menuOptions();
+			choiceText = readTask();
 		try {
-			choice =Integer.parseInt(System.console().readLine());
+			choice =Integer.parseInt(choiceText);
 			if(choice == 1)
 			{
 				addTask();
 			}
 			else if(choice == 2)
 			{
-				
+				listTask();
 			}
 			else
 			{
@@ -46,18 +50,18 @@ public class TaskManager {
 	
 	static void menuOptions()
 	{
-		System.console().flush();
+		
 		System.out.println("Please enter your choice");
 		System.out.println("1. Add new task");
-		System.out.print("2. List tasks");
+		System.out.println("2. List tasks");
 		
 	}
 	
 	static void addTask()
 	{
-		System.console().flush();
+		
 		System.out.println("Add task description or enter 0 to exit");
-		String text = System.console().readLine();
+		String text = readTask();
 		if(text.equals("0"))
 		{
 			menu();
@@ -67,11 +71,63 @@ public class TaskManager {
 			Task t = new Task(idCounter, text);
 			idCounter++;
 			taskList.add(t);
+			System.out.println("New task added with id:" + taskList.size());
+			screenSeparator();
 			addTask();
+		}
+		
+	}
+	
+	static void listTask()
+	{
+		System.out.println("ToDo List");
+		for (int i = 0; i < taskList.size(); i++)
+		{
+			System.out.println(i + 1 + "." + " " + taskList.get(i).getDescription() + "\n");
+		}
+		
+		String choice = readTask();
+		try
+		{
+			int selected = Integer.parseInt(choice);	
+			selected--;
+			while(selected < 0 || selected > taskList.size())
+			{
+				System.out.println("Not an valid id");
+				selected = Integer.parseInt(choice);
+			}
+			
+			taskList.remove(selected);
+			System.out.println("Task removed");
+			
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("Not a valid number");
 		}
 		
 	}
 
 	
 
+	static void screenSeparator(){
+		System.out.println("--------------------------------------------------------------");
+	}
+	
+	static String readTask()
+	{
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	     String text = null;
+	      try {
+	    	  text = br.readLine();
+	      } catch (IOException ioe) {
+	         System.out.println("Error reading input");
+	         
+	      }
+	      
+	      return text;
+	}
+	
+	
 }
